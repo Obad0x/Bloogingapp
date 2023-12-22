0a60c1fe153cc86baa68b9b445563f440dd26905
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2023-12-22 17:00:20 +0100

    updates

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.static('public'))
+
+// Templating Engine
+app.use(expressLayout)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs');
+
+
+
+
+app.use('/', require('./server/Routes/main'))
+
+
+
+
+app.listen(PORT, ()=> console.log(`listening on ${PORT}`))
