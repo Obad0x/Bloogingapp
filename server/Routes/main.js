abfa2f89cf3a52899b856139376823657319c8de
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2023-12-22 20:24:32 +0100

    pagination fix

--- server/Routes/main.js
+++ server/Routes/main.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const router = express.Router(); 
 
-router.get("", (req, res )=>{
-    res.render('index')
+const Post  = require('../models/Post')
+
+
+router.get("", async (req, res )=>{
+try{
+            let perPage = 3;
+            let page = req.query.page || 1 ;
+
+
+            const data = await Post.aggregate({ $sort :{createdAt : -1} }) 
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+
+            const count = await Post.countDocuments({});
+            const nextPage = parseInt(page) + 1;
+            const hasNextPage = nextPage <= Math.ceil(count / perPage)
+
+            res.render('index',
+             {
+                data,
+                current : page ,
+                nextPage : hasNextPage ? nextPage :null
+            })
+
+
+
+}
+catch (error){
+    console.error(error);
+    
+
+}
 })
 
-router.get("/about", (req, res )=>{
+
+
+
+
+
+router.get("/about",  (req, res )=>{
+
+
     res.render('about')
 })
 
