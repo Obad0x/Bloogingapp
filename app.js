ee9cae8d6a1064d347143fa08c1ff299fd613e2c
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2023-12-24 15:45:41 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 const connectDB = require('./server/config/db')
 const app = express()
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+const MongoStore = require('connect-mongo')
 
 const PORT = process.env.PORT || 3000
 
@@ -13,6 +17,15 @@ connectDB();
 app.use(express.urlencoded({extended : true}));
 app.use(express.json())
 app.use(express.static('public'))
+app.use(cookieParser);
+app.use(session({
+    secret : 'keyboard cat',
+    resave : false,
+    saveUninitialized : true,
+    store : MongoStore.create({
+        mongoUrl : process.env.MONGODB_URI
+    }),
+}));
 
 // Templating Engine
 app.use(expressLayout)
--- server/Routes/admin.js
+++ server/Routes/admin.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router(); 
 const adminLayout = '../views/layouts/admin.ejs'
 const Post  = require('../models/Post')
+const User  = require('../models/User')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /*
     GET /
@@ -23,5 +26,67 @@ const Post  = require('../models/Post')
         
                
             })
+
+
+            /*
+    POST/
+    *Admin - Check Login
+
+ */
+
+    
+    router.post("/admin", async  (req, res )=>{
+
+        try {
+            
+            -
+            res.render('admin/index', {layout: adminLayout})
+        } 
+        catch (error) {
+                console.log(error)
+        }
+        
+        
+               
+            })
+
+
+
+
+    /* POST/
+    *Admin - Register
+
+ */
+
+    
+    router.post("/register", async  (req, res )=>{
+
+        try {
+            const {username , password} = req.body;
+            const hashedpassword =await bcrypt.hash(password, 10) ;
+            
+         try {
+                const user = await User.create({username, password : hashedpassword })
+                res.status(201).json({message : 'user Created'. user})
+
+         } catch (error) {
+            if(error === 11000){
+                res.status(409).json({message: ' user already in use', })
+            }
+            res.status(500).json({message : 'internal server error'})
+         }
+
+
+
+
+        } 
+        catch (error) {
+                console.log(error)
+        }
+        
+        
+               
+            })
+
 
 module.exports = router;
